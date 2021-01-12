88b52426e83e7afaee9f59631a95c5b204cc484a
Ralph <19510622+RalXYZ@users.noreply.github.com>
2021-01-12 12:15:02 +0800

    add some other functions

---
+++ README.md
@@ -0,0 +1,3 @@
+# WeChat Mini-program Cloud-Dev Backend Proto
+
+In the `.js` file, you can see some prototypes of CRUD operations.  
--- pages/index/index.js
+++ pages/index/index.js
@@ -34,7 +34,7 @@ Page({
     listID: null, //"ce805e785ff40a4c0350122530c654e5"
   },
 
-  async addSet() {
+  addSet() {
     const db = wx.cloud.database();
     db.collection('set').add({
       data: {
@@ -60,7 +60,7 @@ Page({
     })
   },
 
-  async getSetOpenID() {
+  getSetOpenID() {
     const db = wx.cloud.database();
     db.collection('set').doc(this.data.setID).field({
       _openid: true
@@ -80,7 +80,7 @@ Page({
     })
   },
 
-  async addList() {
+  addList() {
     const db = wx.cloud.database();
     db.collection('list').add({
       data: {
@@ -97,14 +97,76 @@ Page({
     })
   },
 
-  async addSetFinal() {
-    await this.addSet();
-    //console.log(qwerty)
-    await this.getSetOpenID();
-    await this.addList();
-    //await this.getSetOpenID();
-    //await this.addList();
+  addSetFinal() {
+    const db = wx.cloud.database();
+    db.collection('set').add({
+      data: {
+        name: "看电影",
+        description: "一起去看《2001太空漫游》",
+        location: "博纳影院西溪店",
+        date: new Date("2021-01-20 21:30:00"),
+        geo_point: new db.Geo.Point(113.4323, 23.2115),
+        contact: "13901220010",
+      },
+      success: res => {
+        this.setData({
+          setID: res._id
+        })
+        console.log("Add set success");
+        const db = wx.cloud.database();
+        db.collection('set').doc(this.data.setID).field({
+          _openid: true
+        }).get({
+          success: res => {
+            console.log(`Get set openid completed: ${res.data._openid}`)
+            this.setData({
+              setOpenID: res.data._openid
+            })
+            this.addList();
+            console.log(this.data.setOpenID)
+          },
+          fail: err => {
+            console.log("Error: Get set openid failed");
+            console.log(err);
+          }
+        })
+      },
+      fail: err => {
+        console.log("Error: Add set failed");
+        console.log(err);
+      }
+    })
   },
+/*
+  removeSetAndList: function() {
+    const db = wx.cloud.database();
+    db.collection("set").doc(this.data.setID).remove({
+      success: function(res) {
+        console.log("Remove set succeeded");
+        this.removeListBySetID();
+      },
+      fail: function(err) {
+        console.log("Error: Remove set failed");
+        console.log(err);
+      }
+    })
+  },
+
+  removeListBySetID: function() {
+    const db = wx.cloud.database();
+    db.collection("list").where({
+      set_id: this.data.setID
+    }).remove({
+      success: function(res) {
+        console.log("Remove list succeeded")
+      },
+      fail: function(err) {
+        console.log("Error: Remove list failed");
+        console.log(err);
+      }
+    })
+},
+*/
 
   removeList: function() {
     const db = wx.cloud.database();
